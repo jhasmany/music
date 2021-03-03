0483ebda54e18d5e24bcb40ec9a3374a245e8cba
Jhasmany <jhasmanyroquechambi@gmail.com>
2021-03-03 18:13:36 -0400

    Adicion de texto

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -3,6 +3,7 @@
       section.header
         h1 Platzi Music
         h2 Canciones
+        h5 Desarrollado por Jhasmany Roque Chambi
         //- pm-player
       svg(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 60")
         path(fill="#1E5371" fill-opacity="1" d="M0,0L120,5.3C240,11,480,21,720,21.3C960,21,1200,11,1320,5.3L1440,0L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z")
@@ -23,9 +24,12 @@
   padding: 16px;
 }
 
-.header h1, .header h2 {
+.header h1, .header h2, .header h5 {
   margin: 0;
   color: #ffffff;
+}
+.header h5{
+  margin-top: 8px;
 }
 /* .wave {
     position: absolute;
